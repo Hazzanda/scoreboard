66e9785695c8afb61011131799bea0a0c15711e1
Hazzanda <hassan.omolola@gmail.com>
2023-11-21 19:55:10 +0100

    commit initial files

---
+++ index.js
@@ -0,0 +1,37 @@
+let counterH = document.getElementById("homeCounter")
+let counterG = document.getElementById("guestCounter")
+
+
+let countH = 0
+
+function onePointHome() {
+    countH += 1
+    counterH.innerText = countH
+}
+
+function twoPointHome() {
+    countH += 2
+    counterH.innerText = countH
+}
+
+function threePointHome() {
+    countH += 3
+    counterH.innerText = countH
+}
+
+let countG = 0
+
+function onePointGuest() {
+    countG += 1
+    counterG.innerText = countG
+}
+
+function twoPointGuest() {
+    countG += 2
+    counterG.innerText = countG
+}
+
+function threePointGuest() {
+    countG += 3
+    counterG.innerText = countG
+}
